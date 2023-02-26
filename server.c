bbf34705db7d46a7dc99ae39ad04d2ca9d29b163
Hassan Aguezoum <haguezou@c1r3p7.1337.ma>
2023-02-26 22:12:27 +0100

    fix buffer to work with unicode chars like 32bits

--- server.c
+++ server.c
@@ -6,115 +6,86 @@
 /*   By: haguezou <haguezou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 22:30:07 by haguezou          #+#    #+#             */
-/*   Updated: 2023/02/22 15:48:39 by haguezou         ###   ########.fr       */
+/*   Updated: 2023/02/26 18:02:01 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int power(int n)
+int	convert(char *str)
 {
-  int tot;
-  tot = 1;
-  while(n)
-  {
-    tot = tot * 2;
-    n--;
-  }
-  return tot;
+	int	sum;
+	int	i;
+
+	sum = 0;
+	i = 0;
+	while (i < 8)
+	{
+		sum = (sum << 1) | (str[i] == '1');
+		i++;
+	}
+	return (sum);
 }
 
-int convert(unsigned char *str)
+void	print_char(char *bits, int byte)
 {
-    int sum;
-    int i;
-    sum = 0;
-    i = 0;
+	int	i;
 
-    while(str[i])
-    {                                            
-        if(str[i] == '1')
-        {
-            sum = sum + power(7-i);
-        }
-        i++;
-    }
-    return (sum);
+	i = -1;
+	while (++i < byte)
+	{
+		ft_putchar_fd(convert(bits + (i * 8)), 2);
+	}
 }
 
-int convert_b(unsigned char *str)
+void	byte_counter(int *byte, int sum)
 {
-    int sum;
-    int i;
-    sum = 0;
-    i = 0;
-
-    while(str[i])
-    {       
-                                            
-        if(str[i] == '1' && i < 4)
-        {
-            sum = sum + power(7-i);
-        }
-        i++;
-    }
-    return (sum);
+	if (sum == 15)
+		*byte = 4;
+	else if (sum == 14)
+		*byte = 3;
+	else if (sum == 12)
+		*byte = 2;
+	else
+		*byte = 1;
 }
 
-void handler(int sig, siginfo_t *info , void *unsend)
+void	handler(int sig, siginfo_t *info, void *unsend)
 {
-    static unsigned char bits[8];
-    static int byte ;
-    static int senderPID;
-    static int x;
-    static char c = 0;
-    
-    if(senderPID != info->si_pid)
-        byte = 0;
-    senderPID = info->si_pid;
-    if(sig == SIGUSR1)    
-        bits[byte++] = '0';
-    else
-        bits[byte++] = '1';
+	static char		bits[32];
+	static int		byte ;
+	static int		senderPID;
+	static int		x;
+	static int		sum;
 
-    if(byte == 4)
+	if (senderPID != info->si_pid)
     {
-        if(convert_b(bits) == 240)
-            x = 4;
-        else if(convert_b(bits) == 224)
-           x = 3;
-        else if(convert_b(bits) == 192)
-           x = 2;
-        else
-            x = 1;
-    }
-
-    // if(byte == 8)
-    // {  
-        // byte = 0;
-        c = convert(bits);
-        // while (x > 0)
-        // {
-            // str[x] = convert(bits);
-            // x--;
-        // }
-        // ft_printf("%c",convert(bits));
-        ft_putchar_fd(c, 1);
-        c = 0;
-    // }
+		byte = 0;
+		x = 0;
+		ft_bzero(bits, 31);
+		senderPID = info->si_pid;
+	}
+	bits[x] = (sig == SIGUSR2) + 48;
+	sum = (sum << 1) | (sig == SIGUSR2);
+	x++;
+	if	(x == 4)
+		byte_counter(&byte, sum);
+	if(x == (8 * byte))
+	{
+		print_char(bits, byte);
+		sum = 0;
+		x = 0;
+	}
 }
 
-int main()
+int	main()
 {
-    ft_printf("%d\n",getpid());
-    struct sigaction sa;
-    sa.sa_sigaction =  handler;
-    sa.sa_flags = SA_SIGINFO | SA_RESTART;
-    sigaction(SIGUSR1,&sa,NULL);
-    sigaction(SIGUSR2,&sa,NULL);
-    while (1)
-    {
-        pause();
-    }
-    
+	ft_putnbr_fd(getpid(), 2);
+	struct sigaction sa;
+	sa.sa_sigaction =  handler;
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigaction(SIGUSR1,&sa,NULL);
+	sigaction(SIGUSR2,&sa,NULL);
+	while (1)
+		pause();
 }
