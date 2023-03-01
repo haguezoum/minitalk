e83f46fd227f254c9085ac5c27747c8b42490714
Hassan Aguezoum <haguezou@c1r2p9.1337.ma>
2023-03-01 17:26:57 +0100

    add helper function from utility file

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: haguezou <haguezou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 14:48:31 by haguezou          #+#    #+#             */
-/*   Updated: 2023/02/15 14:48:34 by haguezou         ###   ########.fr       */
+/*   Updated: 2023/03/01 16:47:21 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include "libft/libft.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 
+void	ft_putchar_fd(char c, int fd);
+void	print_each_number(unsigned int num, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_bzero(void *string, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *string);
 
 #endif 
