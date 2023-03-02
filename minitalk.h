2317725d9bd5c30dc2f77f00900d2fc3070809d9
Hassan Aguezoum <haguezou@c1r3p9.1337.ma>
2023-03-02 12:43:11 +0100

    fix norminette

--- minitalk.h
+++ minitalk.h
@@ -6,17 +6,16 @@
 /*   By: haguezou <haguezou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 14:48:31 by haguezou          #+#    #+#             */
-/*   Updated: 2023/03/01 16:47:21 by haguezou         ###   ########.fr       */
+/*   Updated: 2023/03/01 22:07:45 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
 
 void	ft_putchar_fd(char c, int fd);
 void	print_each_number(unsigned int num, int fd);
